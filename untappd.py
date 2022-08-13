64741b341d69562798951a6e5f47810289199d4b
Heath Seals <heath@nodata.org>
2022-08-13 11:23:04 -0700

    updates

--- untappd.py
+++ untappd.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python3
-import json
-import sys
-import datetime
-
-if len(sys.argv) < 2 or len(sys.argv) >= 3:
-    print(f"{sys.argv[0]} jsonexport")
-    sys.exit()
-
-file = sys.argv[1]
-
-print(f"Working on {file}")
-
-with open(file) as f:
-  data = json.load(f)
-  f.close
-
-date = datetime.date.today()
+import json, datetime, argparse
 
 def avg_abv(abv_list):
     avg_abv = sum(abv_list) / len(abv_list)
     return round(avg_abv,2)
 
-for y in range(2010, int(date.year) + 1):
+def per_year(data):
+    date = datetime.date.today()
+    for y in range(2010, int(date.year) + 1):
+        checkin = 0
+        abv_list = []
+        for d in data:
+            if d['created_at'].find(str(y)) >= 0:
+                if float(d['beer_abv']) > 0:
+                    abv_list.append(float(d['beer_abv'])) 
+                checkin = checkin + 1
+        print(f"{y} : {max(abv_list)}% max | {avg_abv(abv_list)}% avg | {min(abv_list)}% min | {checkin} unique check-ins")
+
+def all_years(data):
     checkin = 0
     abv_list = []
     for d in data:
-        if d['created_at'].find(str(y)) >= 0:
-            if float(d['beer_abv']) > 0:
-                abv_list.append(float(d['beer_abv'])) 
-            checkin = checkin + 1
-    print(f"{y} : {max(abv_list)}% max | {avg_abv(abv_list)}% avg | {min(abv_list)}% min | {checkin} unique check-ins")
+        if float(d['beer_abv']) > 0:
+            abv_list.append(float(d['beer_abv']))
+        checkin = checkin + 1
+    print(f"All Years : {max(abv_list)}% max | {avg_abv(abv_list)}% avg | {min(abv_list)}% min | {checkin} unique check-ins")
 
-checkin = 0
-abv_list = []
-for d in data:
-    if float(d['beer_abv']) > 0:
-        abv_list.append(float(d['beer_abv'])) 
-    checkin = checkin + 1
-print(f"All Years : {max(abv_list)}% max | {avg_abv(abv_list)}% avg | {min(abv_list)}% min | {checkin} unique check-ins")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='name of the json file to extract uniques from')
+    args = parser.parse_args()
+
+    with open(args.file) as f:
+        data = json.load(f)
+        f.close
+
+    print(f"Working on {args.file}")
+
+    per_year(data)
+    all_years(data)
